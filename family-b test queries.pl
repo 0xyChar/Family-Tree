fbb09fdb3a1d9b98157c5d34740bbad8b28af99d
0xyChar <charityymutua@gmail.com>
2026-05-16 01:15:52 +0300

    Create family-b test queries.pl

---
+++ family-b test queries.pl
@@ -0,0 +1,7 @@
+?- [family_b].
+?- father(X, mwendwa).      % Who is father of mwendwa?
+?- mother(X, mwendwa).      % Who is mother of mwendwa?
+?- sibling(mwendwa, X).     % Who are mwendwa's siblings?
+?- brother(X, ndanu).       % Who are brothers of ndanu?
+?- sister(X, mwendwa).      % Who are sisters of mwendwa?
+?- child(X, masaku).        % Who are children of masaku?
